35bb74f4d980bcbb1eb5b514f4f544c7f88c99b6
Adhy <adhywiranatap@gmail.com>
2017-04-14 00:02:03 +0700

    003 updated module

--- 003-sum-and-range.js
+++ 003-sum-and-range.js
@@ -3,3 +3,7 @@ const R = require('ramda');
 console.log(R.range(1, 5)); // [1, 2, 3, 4]
 console.log(R.sum([1, 2, 3, 4])); // 10
 console.log(R.sum(R.range(1, 5))); // 10
+
+// example of currying a range
+var from10ToExclusive = R.range(10);
+console.log(from10ToExclusive(15)); //=> [10, 11, 12, 13, 14]
